1689b3972dbc6d34f7200ce999f331f0b95e5061
Nikos Fertakis <nikos.fertakis@gmail.com>
2017-02-22 20:11:45 +0200

    Validate Article
    - Try to validat URL format
    - Require at least `url` or `text` field to be present (or both).

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,10 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 5 }
+  validates :url,
+            format: { with: URI::regexp(%w(http https)),
+                      allow_blank: true,
+                      message: "\"%{value}\" does't seem like a valid URL" }
+  validates :url, presence: true, if: "text.blank?"
+  validates :text, presence: true, if: "url.blank?"
 end
