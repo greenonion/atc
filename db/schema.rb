f6334a1341b533551727b30aa05f78fc1032e086
Nikos Fertakis <nikos.fertakis@gmail.com>
2017-02-22 20:11:25 +0200

    Add url field to article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205203143) do
+ActiveRecord::Schema.define(version: 20161206181856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161205203143) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "url"
     t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
   end
 
