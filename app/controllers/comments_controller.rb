c9b6b35d7277fa2c5bde172cad29a79ec0d6afda
Nikos Fertakis <nikos.fertakis@gmail.com>
2017-03-08 22:11:55 +0200

    Set current_user as the user of a new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    @comment = @article.comments.build(comment_params)
+    @comment.user = current_user
+
+    @article.save
 
     redirect_to article_path(@article)
   end
