8ddb72b23761288b3b26cacfd8f0345aec477154
Nikos Fertakis <nikos.fertakis@gmail.com>
2017-03-08 22:11:18 +0200

    Remove commenter column from comments table
    We now have a user_id foreign key.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308182935) do
+ActiveRecord::Schema.define(version: 20170308200913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20170308182935) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
     t.text     "body"
     t.integer  "article_id"
     t.datetime "created_at", null: false
